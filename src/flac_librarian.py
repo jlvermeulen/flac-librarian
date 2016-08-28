7a61bb24f212750fae2e8f91a88f3cb9fe6756d5
Jordi Vermeulen <jordi@jordivermeulen.nl>
2016-08-28 16:50:07 +0200

    Fix issue with subprocess.run not working when double-clicking exe

--- src/flac_librarian.py
+++ src/flac_librarian.py
@@ -2,31 +2,35 @@
 
 import tkinter as tk
 import icon, metaflac
-
-_app = None
+import os
 
 class MainWindow(tk.Frame):
-    frame = None
-    labels_initialised = False
-
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
         self.parent = parent
         self.init()
 
     def init(self):
+        self.labels_initialised = False
+
         self.parent.title('FLAC Librarian 0.1')
         self.pack(fill = tk.BOTH, expand = 1)
 
         self.frame = tk.Frame(self)
         self.frame.pack(fill = tk.BOTH, expand = 1)
 
-        button = tk.Button(self.frame, text = "Read data", command = self.show_data).grid(row = 0)
+        self.button = tk.Button(self.frame, text = "Read data", command = self.show_data)
+        self.button.grid(row = 0)
 
-        track_label  = tk.Label(self.frame, text = 'Track number:').grid(row = 0, column = 1, sticky = 'w')
-        title_label  = tk.Label(self.frame, text = 'Title:').grid(row = 1, column = 1, sticky = 'w')
-        artist_label = tk.Label(self.frame, text = 'Artist:').grid(row = 2, column = 1, sticky = 'w')
-        album_label  = tk.Label(self.frame, text = 'Album:').grid(row = 3, column = 1, sticky = 'w')
+        self.track_label  = tk.Label(self.frame, text = 'Track number:')
+        self.title_label  = tk.Label(self.frame, text = 'Title:')
+        self.artist_label = tk.Label(self.frame, text = 'Artist:')
+        self.album_label  = tk.Label(self.frame, text = 'Album:')
+
+        self.track_label.grid(row = 0, column = 1, sticky = 'w')
+        self.title_label.grid(row = 1, column = 1, sticky = 'w')
+        self.artist_label.grid(row = 2, column = 1, sticky = 'w')
+        self.album_label.grid(row = 3, column = 1, sticky = 'w')
 
     def show_data(self):
         if self.labels_initialised:
@@ -37,10 +41,15 @@ class MainWindow(tk.Frame):
         album = metaflac.get_attribute('album', 'test.flac')
         tracknumber = metaflac.get_attribute('tracknumber', 'test.flac')
 
-        track_label  = tk.Label(self.frame, text = tracknumber).grid(row = 0, column = 2, sticky = 'w')
-        title_label  = tk.Label(self.frame, text = title).grid(row = 1, column = 2, sticky = 'w')
-        artist_label = tk.Label(self.frame, text = artist).grid(row = 2, column = 2, sticky = 'w')
-        album_label  = tk.Label(self.frame, text = album).grid(row = 3, column = 2, sticky = 'w')
+        self.track_value  = tk.Label(self.frame, text = tracknumber)
+        self.title_value  = tk.Label(self.frame, text = title)
+        self.artist_value = tk.Label(self.frame, text = artist)
+        self.album_value  = tk.Label(self.frame, text = album)
+
+        self.track_value.grid(row = 0, column = 2, sticky = 'w')
+        self.title_value.grid(row = 1, column = 2, sticky = 'w')
+        self.artist_value.grid(row = 2, column = 2, sticky = 'w')
+        self.album_value.grid(row = 3, column = 2, sticky = 'w')
 
         self.labels_initialised = True
 
@@ -48,8 +57,7 @@ if __name__ == '__main__':
     root = tk.Tk()
     root.geometry('1280x720+200+200')
 
-    global _app
-    _app = MainWindow(root)
+    app = MainWindow(root)
 
     icon.create_icon()
     root.iconbitmap('flac-librarian.ico')
--- src/metaflac.py
+++ src/metaflac.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python3
 
-import subprocess
+import subprocess, os.path
 
 def get_attribute(name, file):
-    result = subprocess.run(['metaflac', '--show-tag=' + name, file], stdout = subprocess.PIPE, shell = True)
+    result = subprocess.run(['metaflac', '--show-tag=' + name, file], stdout = subprocess.PIPE, stderr = subprocess.PIPE, stdin = subprocess.PIPE, shell = True)
     value  = result.stdout.decode('utf-8')
+
+    assert result.returncode == 0
 
     assert value.startswith(name + '=')
     return value[len(name) + 1:].rstrip()
